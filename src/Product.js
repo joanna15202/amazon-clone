d93afb2d47812b3b2ced8fec734d50144cbab11e
Joanna <Joanna@USERs-MacBook.local>
2021-07-07 19:43:18 +0800

    Fix bug

--- src/Product.js
+++ src/Product.js
@@ -4,11 +4,7 @@ import GradeIcon from "@material-ui/icons/Grade";
 import ShoppingCartIcon from "@material-ui/icons/ShoppingCart";
 import { useStateValue } from "./StateProvider";
 
-<<<<<<< HEAD
 function Product({ id, title, price, image, rating, description }) {
-=======
-function Product({ id, title, price, image, rating }) {
->>>>>>> d8a1cf83e2e3893a26487cd0442755da3588f2a8
   const [state, dispatch] = useStateValue();
 
   const addToBasket = () => {
@@ -21,17 +17,13 @@ function Product({ id, title, price, image, rating }) {
         image: image,
         price: price,
         rating: rating,
-<<<<<<< HEAD
         description: description,
-=======
->>>>>>> d8a1cf83e2e3893a26487cd0442755da3588f2a8
       },
     });
   };
 
   return (
     <div className="product">
-<<<<<<< HEAD
       {/* Image */}
       <img src={image} alt="" />
 
@@ -41,11 +33,6 @@ function Product({ id, title, price, image, rating }) {
         <p className="product__description">
           {description.substring(0, 150) + " ..."}
         </p>
-=======
-      {/* Title/Price/Star Rating */}
-      <div className="product__info">
-        <p>{title}</p>
->>>>>>> d8a1cf83e2e3893a26487cd0442755da3588f2a8
         <p className="product__price">
           <small>$</small>
           <strong>{price}</strong>
@@ -61,19 +48,11 @@ function Product({ id, title, price, image, rating }) {
             ))}
         </div>
       </div>
-<<<<<<< HEAD
 
       {/* Add to Basket */}
       <button onClick={addToBasket} className="btn btn-sm col-12 btn-warning">
         <ShoppingCartIcon className="product__shoppintCartIcon" />
         <span>Add to Cart</span>
-=======
-      {/* Image */}
-      <img src={image} alt="" />
-      {/* Add to Basket */}
-      <button onClick={addToBasket} className="btn btn-warning btn-sm">
-        <ShoppingCartIcon className="product__shoppintCartIcon" /> Add to Cart
->>>>>>> d8a1cf83e2e3893a26487cd0442755da3588f2a8
       </button>
     </div>
   );
